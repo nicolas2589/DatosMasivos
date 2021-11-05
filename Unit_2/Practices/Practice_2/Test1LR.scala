1299ae08fdd027abe23e42b646cfe445102ef9cd
Ariel <Ariel.landa17@tectijuana.edu.mx>
2021-11-04 20:24:16 -0700

    Arreglar practica 2 Regresion lineal

--- Unit_2/Practices/Practice_2/Test1LR.scala
+++ Unit_2/Practices/Practice_2/Test1LR.scala
@@ -1,21 +1,21 @@
-import org.apache.spark.ml.classification.LogisticRegression
+import org.apache.spark.ml.regression.LinearRegression
 
 // Load training data
 val training = spark.read.format("libsvm").load("sample.txt")
 
-val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+
+val lr = new LinearRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
 
 // Fit the model
 val lrModel = lr.fit(training)
 
-// Print the coefficients and intercept for logistic regression
+// Print the coefficients and intercept for linear regression
 println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
 
-// We can also use the multinomial family for binary classification
-val mlr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8).setFamily("multinomial")
-
-val mlrModel = mlr.fit(training)
-
-// Print the coefficients and intercepts for logistic regression with multinomial family
-println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
-println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
+// Summarize the model over the training set and print out some metrics
+val trainingSummary = lrModel.summary
+println(s"numIterations: ${trainingSummary.totalIterations}")
+println(s"objectiveHistory: [${trainingSummary.objectiveHistory.mkString(",")}]")
+trainingSummary.residuals.show()
+println(s"RMSE: ${trainingSummary.rootMeanSquaredError}")
+println(s"r2: ${trainingSummary.r2}")
