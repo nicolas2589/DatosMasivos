3b4e538527347fe155d6813144cef57021fa1b37
Ariel <Ariel.landa17@tectijuana.edu.mx>
2021-11-04 17:46:43 -0700

    Practica 2 Regresion lineal

---
+++ Unit_2/Practices/Practice_2/Test1LR.scala
@@ -0,0 +1,21 @@
+import org.apache.spark.ml.classification.LogisticRegression
+
+// Load training data
+val training = spark.read.format("libsvm").load("sample.txt")
+
+val lr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8)
+
+// Fit the model
+val lrModel = lr.fit(training)
+
+// Print the coefficients and intercept for logistic regression
+println(s"Coefficients: ${lrModel.coefficients} Intercept: ${lrModel.intercept}")
+
+// We can also use the multinomial family for binary classification
+val mlr = new LogisticRegression().setMaxIter(10).setRegParam(0.3).setElasticNetParam(0.8).setFamily("multinomial")
+
+val mlrModel = mlr.fit(training)
+
+// Print the coefficients and intercepts for logistic regression with multinomial family
+println(s"Multinomial coefficients: ${mlrModel.coefficientMatrix}")
+println(s"Multinomial intercepts: ${mlrModel.interceptVector}")
